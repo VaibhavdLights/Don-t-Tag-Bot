87b9bbf2d64960cc4167d2785c41cbb092fe3865
Vaibhav_dLLights <vaibhav.pr21@iiits.in>
2022-04-04 14:36:01 +0530

    updated

--- main.py
+++ main.py
@@ -13,13 +13,21 @@ client = discord.Client()
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
+def gaali(r1):
+    r2 = random.randint(0,len(myList)-1)
+    if(r1!=r2):
+        return r2
+    else:
+        gaali(r1)
+
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
     if len(message.mentions)>0 and message.mentions[0].id == 553146370844917762:
-        r = random.randint(0,len(myList)-1)
-        await message.reply(myList[r] +" "+ message.author + " Tag Mat Kar")
+        r1 = random.randint(0,len(myList)-1)
+        r2 = gaali(r1)
+        await message.reply("**"+myList[r1] +"** "+ message.author.mention + " Tag Mat Kar **"+myList[r2]+"**")
 
 client.run(os.getenv('TOKEN'))
